7c714b5722f7a5fff9e81462d0a6869bc94ff74c
Luka Dornhecker <luka.dornhecker@gmail.com>
2018-08-12 08:31:18 +0200

    update combine

--- src/parser.rs
+++ src/parser.rs
@@ -1,27 +1,27 @@
-use ast::*;
 use ast::Instruction::*;
+use ast::*;
 
-use combine::*;
 use combine::char::char;
-use combine::primitives::{Stream};
+use combine::{between, choice, many, many1, satisfy, skip_many, Parser, Stream};
 
 parser!{
+    #[inline(always)]
     fn program[I]()(I) -> Program
         where [I: Stream<Item=char>]
     {
         let comments = || skip_many(satisfy(|c| !"+-><,.[]".chars().any(|t| t == c)));
-        let chars    = |c| many1(char(c));
+        let chars = |c| many1(char(c));
 
-        let add   = chars('+').map(|s: String| Add(s.len() as i64));
-        let sub   = chars('-').map(|s: String| Add(-(s.len() as i64)));
+        let add = chars('+').map(|s: String| Add(s.len() as i64));
+        let sub = chars('-').map(|s: String| Add(-(s.len() as i64)));
+        let left = chars('<').map(|s: String| Move(-(s.len() as i64)));
         let right = chars('>').map(|s: String| Move(s.len() as i64));
-        let left  = chars('<').map(|s: String| Move(-(s.len() as i64)));
-        let read  = char(',').map(|_: char| Read);
+        let read = char(',').map(|_: char| Read);
         let write = char('.').map(|_: char| Write);
 
-        let bfloop = between(char('['), char(']'), program()).map(|prog| Loop(prog));
+        let bfloop = between(char('['), char(']'), program()).map(Loop);
 
-        let instruction = choice!(
+        let instruction = choice((
             add,
             sub,
             left,
@@ -29,7 +29,7 @@ parser!{
             read,
             write,
             bfloop
-        ).skip(comments());
+        )).skip(comments());
 
         (
             comments(),
@@ -40,20 +40,20 @@ parser!{
 
 fn optimize(program: Program) -> Program {
     match *program {
-        []                             => vec![],
-        [Add(0),           ref rest..] => optimize(rest.to_vec()),
-        [Move(0),          ref rest..] => optimize(rest.to_vec()),
+        [] => vec![],
+        [Add(0), ref rest..] => optimize(rest.to_vec()),
+        [Move(0), ref rest..] => optimize(rest.to_vec()),
 
-        [Add(a), Add(b),   ref rest..] => {
+        [Add(a), Add(b), ref rest..] => {
             let mut v = optimize(rest.to_vec());
             v.insert(0, Add(a + b));
             v
-        },
+        }
         [Move(a), Move(b), ref rest..] => {
             let mut v = optimize(rest.to_vec());
             v.insert(0, Move(a + b));
             v
-        },
+        }
 
         [Loop(ref body), ref rest..] => {
             let mut v: Program = vec![];
@@ -62,28 +62,28 @@ fn optimize(program: Program) -> Program {
             }
             v.append(&mut optimize(rest.to_vec()));
             v
-        },
+        }
 
         [Set(a), Add(b), ref rest..] => {
             let mut v = optimize(rest.to_vec());
             v.insert(0, Set(a + b));
             v
-        },
+        }
         [Add(_), Set(b), ref rest..] => {
             let mut v = optimize(rest.to_vec());
             v.insert(0, Set(b));
             v
-        },
+        }
         [Set(_), Set(x), ref rest..] => {
             let mut v = optimize(rest.to_vec());
             v.insert(0, Set(x));
             v
-        },
+        }
         [Set(0), Loop(_), ref rest..] => {
             let mut v = optimize(rest.to_vec());
             v.insert(0, Set(0));
             v
-        },
+        }
 
         [ref ins, ref rest..] => {
             let mut v = optimize(rest.to_vec());
@@ -95,10 +95,10 @@ fn optimize(program: Program) -> Program {
 
 fn optimize_loop(program: Program) -> Option<Program> {
     match *program {
-        []         => None,
-        [Add (-1)] => Some(vec![Set(0)]),
-        [Move (n)] => Some(vec![Scan(n)]),
-        _          => Some(vec![Loop(optimize(program))])
+        [] => None,
+        [Add(-1)] => Some(vec![Set(0)]),
+        [Move(n)] => Some(vec![Scan(n)]),
+        _ => Some(vec![Loop(optimize(program))]),
     }
 }
 
@@ -116,6 +116,6 @@ fn opt(program: Program) -> Program {
 
 /// Transform source input into a [Program](type.Program.html).
 pub fn parse(input: &str) -> Program {
-    let (prog, _state) = program().parse(State::new(input)).unwrap();
+    let (prog, _state) = program().parse(input).unwrap();
     opt(prog)
 }
