9a446f195d7393d121c393d8b7d899254b699666
Luka Dornhecker <luka.dornhecker@toptranslation.com>
2018-11-04 22:02:44 +0100

    style: run cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,10 @@ use std::io::{self, Read};
 
 use rbf::parse;
 
-fn read_source<R>(mut input: R) -> String where R: Read {
+fn read_source<R>(mut input: R) -> String
+where
+    R: Read,
+{
     let mut source = String::new();
     input.read_to_string(&mut source).unwrap();
     source
@@ -15,7 +18,7 @@ fn read_source<R>(mut input: R) -> String where R: Read {
 fn main() {
     let source = match env::args().nth(1) {
         Some(file) => read_source(File::open(file).unwrap()),
-        None => read_source(io::stdin())
+        None => read_source(io::stdin()),
     };
 
     println!("{:?}", parse(source.as_str()))
