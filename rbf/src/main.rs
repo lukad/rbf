a5314905429b96703624c66d369426cc784df15d
Luka Dornhecker <luka.dornhecker@gmail.com>
2019-02-13 23:17:36 +0100

    add CLI with clap

--- rbf/src/main.rs
+++ rbf/src/main.rs
@@ -1,27 +1,56 @@
+extern crate clap;
 extern crate librbf;
 
-use std::env;
 use std::fs::File;
 use std::io::{self, Read};
 
 use librbf::{parse, Jit};
 
-fn read_source<R>(mut input: R) -> String
-where
-    R: Read,
-{
+use clap::{App, AppSettings, Arg};
+
+fn read_source(path: &str) -> io::Result<String> {
+    let mut file = File::open(path)?;
     let mut source = String::new();
-    input.read_to_string(&mut source).unwrap();
-    source
+    file.read_to_string(&mut source)?;
+    Ok(source)
 }
 
 fn main() {
-    let source = match env::args().nth(1) {
-        Some(file) => read_source(File::open(file).unwrap()),
-        None => read_source(io::stdin()),
-    };
+    let matches = App::new(env!("CARGO_PKG_NAME"))
+        .version(env!("CARGO_PKG_VERSION"))
+        .about(env!("CARGO_PKG_DESCRIPTION"))
+        .setting(AppSettings::ColoredHelp)
+        .arg(
+            Arg::with_name("PROGRAM")
+                .help("The program")
+                .takes_value(true)
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("emit")
+                .short("e")
+                .value_name("EMIT")
+                .help("Shows the program instead of running it")
+                .possible_value("ast")
+                .takes_value(true),
+        )
+        .get_matches();
 
+    let source_path = matches.value_of("PROGRAM").unwrap();
+    let source = match read_source(source_path) {
+        Ok(source) => source,
+        Err(a) => {
+            eprintln!("Could not read program: {}", a);
+            std::process::exit(1);
+        }
+    };
     let program = parse(source.as_str());
+
+    if let Some("ast") = matches.value_of("emit") {
+        println!("{:?}", program);
+        return;
+    }
+
     let mut jit = Jit::allocate();
     jit.generate(&program);
     jit.run();
